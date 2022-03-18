23090ee18dc929ea4fd0602afd8fbe25594b1f6f
Connor Marshall <cm210@hw.ac.uk>
2022-03-18 20:30:06 +0000

    Player and Ship controls integrated
    Includes the Jupiter + Europa skybox and a few alternatives

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
 	
-	public float speed = 50f;
+	// forward / backward
+	public float thrust = 1000f;
+	// up / down
+	public float liftThrust = 1000f;
+	// left / right
+	public float strafeThrust = 1000f;
+	// pitch (mouse Y)
+	public float pitchTorque = 300f;
+	// yaw (mouse X)
+	public float yawTorque = 300f;
 	
-	public Transform hipTrans;
-	public ConfigurableJoint hipJoint;
-	public Rigidbody hipRigid;
 	
+	/* Glide reduction / drag for ships to slow down automatically,
+	 * but for the character we want the danger of floating indefinitely
+	 * to be a mechanic.
+	 */
+	 
+	private float thrustInput;
+	private float liftInput;
+	private float strafeInput;
+	private Vector2 mouseInput;
+	
+	
+	// References for the Pelivs
+	public Transform pelvisTrans;
+	public ConfigurableJoint pelivsConfigJoint;
+	public Rigidbody pelvisRigidbody;
+	
+	// Animator of the animation model
 	public Animator targetAnimator;
-	private bool moving = false;
 	
+	// Main camera
 	public Transform cam;
+	// Transform of cam follow target
+	public Transform rootTrans;
+	
+	private float pitch = 0.0f;
+	private float yaw = 0.0f;
 	
     // Start is called before the first frame update
     void Start()
@@ -23,42 +52,69 @@ public class CharacterControl : MonoBehaviour
 		Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-		// Move hips based on inputs
-        float horizontal = Input.GetAxisRaw("Horizontal");
-		float vertical = Input.GetAxisRaw("Vertical");
-		
-		// Problem with the prefab's pelvis rotation, guy has the mesh rotated wrong.
-		// Fix with blender, import the mesh, rotate it right, reimport.
-		Vector3 movement = vertical * hipTrans.up + horizontal * -hipTrans.forward;
-		
-		// If moving
-		if(movement.magnitude >= 0.1f)
-		{
-			this.moving = true;
-			this.hipRigid.AddForce(movement.normalized * this.speed);
-		
-		} else {
-			this.moving = false;
-		}
-		
-		// Trigger animations
-		
-		this.targetAnimator.SetBool("Run", this.moving);
-		
-		// Rotate to camera
-		Quaternion camForward = Quaternion.LookRotation(cam.forward);
-		
-		this.hipJoint.targetRotation = new Quaternion(
-			camForward.y,
-			0f,
-			0f,
-			camForward.w
-		);
+	// Update is called once per frame
+	void Update()
+	{
+		yaw += mouseInput.x * yawTorque;
+		pitch -= mouseInput.y * pitchTorque;
 		
 		
+		/* 360 rotation is susceptible to gimbal lock. Unity applies code in reverse order
+		 * e.g. 3rd * 2nd * 1st. Since we are not applying roll to the character on the
+		 * z axis, by applying it 3rd in the sum Gimbal lock is constrained to the unused
+		 * z axis of rotation.
+		 *
+		 * This gimbal lock issue I suspect is to do with the bad pelvis mesh rotation again,
+		 * which is also why pitch is around the z axis and yaw around the x.
+		 */
+		this.pelivsConfigJoint.targetRotation = Quaternion.AngleAxis(0, Vector3.up)
+			* Quaternion.AngleAxis(pitch, Vector3.forward)
+			* Quaternion.AngleAxis(yaw, Vector3.right);
 			
+	}
+   
+    void FixedUpdate()
+    {
+		UpdateMovement();
     }
+	
+	void UpdateMovement() {
+		
+		// Thrust
+		if(thrustInput > 0.1f || thrustInput < -0.1f)
+			pelvisRigidbody.AddRelativeForce(Vector3.up * thrustInput * thrust * Time.deltaTime);
+		// Strafe
+		if(strafeInput > 0.1f || strafeInput < -0.1f)
+			pelvisRigidbody.AddRelativeForce(-Vector3.forward * strafeInput * strafeThrust * Time.deltaTime);
+		// Lift
+		if(liftInput > 0.1f || liftInput < -0.1f)
+			pelvisRigidbody.AddRelativeForce(-Vector3.right * liftInput * liftThrust * Time.deltaTime);
+		
+		
+	}
+	
+	
+	
+	// Input System callbacks
+	public void OnThrust(InputAction.CallbackContext context)
+	{
+		thrustInput = context.ReadValue<float>();
+	}
+	
+	public void OnStrafe(InputAction.CallbackContext context)
+	{
+		strafeInput = context.ReadValue<float>();
+	}
+	
+	public void OnLift(InputAction.CallbackContext context)
+	{
+		liftInput = context.ReadValue<float>();
+	}
+	
+	public void OnMouse(InputAction.CallbackContext context)
+	{
+		mouseInput = context.ReadValue<Vector2>();
+	}
+	
+	
 }
--- Assets/Scripts/SpaceController.cs
+++ Assets/Scripts/SpaceController.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class SpaceController : MonoBehaviour
 {
     //Setting speed related movement speeds for the player ship
-    public float forwardSpeed = 20f;
-    public float hoverSpeed = 4.5f;                                 //NOTE FOR GROUP: Editable to suit final speed preferences. - love Brendan
+    public float thrustSpeed = 20f;
+    public float liftSpeed = 4.5f;                                 //NOTE FOR GROUP: Editable to suit final speed preferences. - love Brendan
     public float strafeSpeed = 6f;
 
     //Stores the input by the user whilst actively moving
-    private float activeForwardSpeed; 
-    private float activeHoverSpeed;
+    private float activeThrustSpeed; 
+    private float activeLiftSpeed;
     private float activeStrafeSpeed;
 
-    private float forwardAcceleration = 2.5f;           //how fast the ship moves from 0-1mph etc
+    private float thrustAcceleration = 2.5f;           //how fast the ship moves from 0-1mph etc
     private float strafeAcceleration = 2f;              //want this to take half a second to accelerate
-    private float hoverAcceleration = 2f;
+    private float liftAcceleration = 2f;
+	
+	// Stores input values from the ShipController action map
+	private float thrustInput = 0f;
+	private float strafeInput = 0f;
+	private float liftInput = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,13 +34,31 @@ public class SpaceController : MonoBehaviour
     void Update()
     {
         //gets input for axes when moving the ship
-        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed,Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);  //allows for acceleration in the ship
-        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime); 
-        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+        activeThrustSpeed = Mathf.Lerp(activeThrustSpeed, thrustInput * thrustSpeed, thrustAcceleration * Time.deltaTime);  //allows for acceleration in the ship
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, strafeInput * strafeSpeed, strafeAcceleration * Time.deltaTime); 
+        activeLiftSpeed = Mathf.Lerp(activeLiftSpeed, liftInput * liftSpeed, liftAcceleration * Time.deltaTime);
 
         //applying the input to the ship (movement)
-        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime; //always goes forward based on rotation of the ship
+        transform.position += transform.forward * activeThrustSpeed * Time.deltaTime; //always goes thrust based on rotation of the ship
         transform.position += transform.right * activeStrafeSpeed * Time.deltaTime;    //same but for left and right
-        transform.position -= transform.up * activeHoverSpeed * Time.deltaTime;   //same but for hovering
+        transform.position += transform.up * activeLiftSpeed * Time.deltaTime;   //same but for Lifting
     }
+	
+	
+	// Input System callbacks
+	public void OnThrust(InputAction.CallbackContext context)
+	{
+		thrustInput = context.ReadValue<float>();
+	}
+	
+	public void OnStrafe(InputAction.CallbackContext context)
+	{
+		strafeInput = context.ReadValue<float>();
+	}
+	
+	public void OnLift(InputAction.CallbackContext context)
+	{
+		liftInput = context.ReadValue<float>();
+	}
+	
 }
